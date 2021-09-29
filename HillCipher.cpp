0cb67535be9cdc97df9ed88db73c44c0be59cf83
Tristan Erney <terney@live.esu.edu>
2021-09-29 16:52:56 -0400

    Updated HillCipher.cpp

--- HillCipher.cpp
+++ HillCipher.cpp
@@ -1,4 +1,7 @@
 #include <iostream>
+#include <iomanip>
+#include <string.h>
+#include <stdlib.h>
 
 int mod(int x, int m) {
   if (x < 0) {
@@ -9,23 +12,33 @@ int mod(int x, int m) {
   }
 }
 
+int mult_inv(int i) {
+  int k = 0;
+  int j = 0;
+  for (j = 0; j < 26; j += 1) {
+    k = (i * j);
+    if (k % 26 == 1) break;
+  }
+  return j;
+}
+
 struct Mat22 {
-  char data[2][2];
+  int data[2][2];
 };
 
-Mat22* mat22_set_key(int a1, int a2, int b1, int b2) {
+Mat22* mat22_set(int a, int b, int c, int d) {
   Mat22* m = (Mat22*)malloc(sizeof(Mat22));
-  m->data[0][0] = a1;
-  m->data[0][1] = a2;
-  m->data[1][0] = b1;
-  m->data[1][1] = b2;
+  m->data[0][0] = a;
+  m->data[0][1] = b;
+  m->data[1][0] = c;
+  m->data[1][1] = d;
   return m;
 }
 
 void mat22_print(Mat22* m) {
-  std::cout  << "[" << (char)(m->data[0][0] + 'A') << " " << (char)(m->data[0][1] + 'A') << "]";
+  std::cout  << "|" << std::setw(2) << (char)(m->data[0][0] + 'A') << " " << std::setw(2) << (char)(m->data[0][1] + 'A') << "|";
   std::cout  << "\n";
-  std::cout  << "[" << (char)(m->data[1][0] + 'A') << " " << (char)(m->data[1][1] + 'A') << "]";
+  std::cout  << "|" << std::setw(2) << (char)(m->data[1][0] + 'A') << " " << std::setw(2) << (char)(m->data[1][1] + 'A') << "|";
   std::cout  << "\n";
 }
 
@@ -40,33 +53,168 @@ Mat22* mat22_mul(Mat22* m, Mat22* n) {
   return o;
 }
 
+Mat22* mat22_adj(Mat22* m) {
+  Mat22* adj = (Mat22*)malloc(sizeof(Mat22));
+  adj->data[0][0] = m->data[1][1];
+  adj->data[0][1] = m->data[0][1] * -1;
+  adj->data[1][0] = m->data[1][0] * -1;
+  adj->data[1][1] = m->data[0][0];
+
+  return adj;
+}
+
 // inv = (1/(ad - bc))[a1 a2]
 //                    [b1 b2]
 Mat22* mat22_inv(Mat22* m) {
-  Mat22* inv = (Mat22*)malloc(sizeof(Mat22));
-  int denom = mod(((m->data[0][0] * m->data[1][1]) - (m->data[0][1] * m->data[1][0])), 26);
-  inv->data[0][0] = mod(-(m->data[1][1]) * denom, 26);
-  inv->data[0][1] = mod(m->data[1][0] * denom, 26);
-  inv->data[1][0] = mod(m->data[0][1] * denom, 26);
-  inv->data[1][1] = mod(-(m->data[0][0]) * denom, 26);
+  Mat22* inv = mat22_adj(m);
+
+  int denom   = mod((m->data[0][0] * m->data[1][1]) - (m->data[0][1] * m->data[1][0]), 26);
+  int inverse = mult_inv(denom);
+
+  inv->data[0][0] = mod(inv->data[0][0] * inverse, 26);
+  inv->data[0][1] = mod(inv->data[0][1] * inverse, 26);
+  inv->data[1][0] = mod(inv->data[1][0] * inverse, 26);
+  inv->data[1][1] = mod(inv->data[1][1] * inverse, 26);
+
   return inv;
+}
+
+// C = E(P, K) = P * K (mod 26)
+char* hillcipher_encrypt(char* P, Mat22* K) {
+  int p_size = (int)strlen(P);
+  char* C = (char*)malloc(sizeof(char) * p_size + 1);
+  C[p_size] = 0;
+
+  // encode P with K
+  int i = 0;
+  int j = 0;
+  for (i = 0; i < p_size;) {
+    Mat22* p_mat = mat22_set(P[i++] - 'a',
+                             (i < p_size) ? P[i++] - 'a' : -1,
+                             (i < p_size) ? P[i++] - 'a' : -1,
+                             (i < p_size) ? P[i++] - 'a' : -1);
+    Mat22* c_mat = mat22_mul(p_mat, K);
+    std::cout << "c_mat on round " << i << " ::\n";
+    mat22_print(c_mat);
+    std::cout << "\n";
+
+    if (p_mat->data[0][0] >= 0) C[j++] = c_mat->data[0][0] + 'A';
+    if (p_mat->data[0][1] >= 0) C[j++] = c_mat->data[0][1] + 'A';
+    if (p_mat->data[1][0] >= 0) C[j++] = c_mat->data[1][0] + 'A';
+    if (p_mat->data[1][1] >= 0) C[j++] = c_mat->data[1][1] + 'A';
+
+    free(p_mat);
+    free(c_mat);
+  }
+
+  return C;
+}
+
+char* hillcipher_decrypt(char* C, Mat22* K) {
+  int c_size = (int)strlen(C);
+  char* P = (char*)malloc(sizeof(char)* c_size + 1);
+  P[c_size] = 0;
+
+  // get inv_K
+  Mat22* inv_K = mat22_inv(K);
+  int i = 0;
+  int j = 0;
+  for (; i < c_size;) {
+    Mat22* c_mat = mat22_set(C[i++] - 'A',
+                             (i < c_size) ? C[i++] - 'A' : -1,
+                             (i < c_size) ? C[i++] - 'A' : -1,
+                             (i < c_size) ? C[i++] - 'A' : -1);
+    Mat22* p_mat = mat22_mul(c_mat, inv_K);
+
+    if (c_mat->data[0][0] >= 0) P[j++] = c_mat->data[0][0] + 'a';
+    if (c_mat->data[0][1] >= 0) P[j++] = c_mat->data[0][1] + 'a';
+    if (c_mat->data[1][0] >= 0) P[j++] = c_mat->data[1][0] + 'a';
+    if (c_mat->data[1][1] >= 0) P[j++] = c_mat->data[1][1] + 'a';
+
+    free(c_mat);
+    free(p_mat);
+  }
+
+  return P;
 }
 
 int main() {
 
-  Mat22* K = mat22_set_key(5, 8, 17, 3);
-  Mat22* inv_K = mat22_inv(K);
+  {
+    Mat22* K = mat22_set(9, 4, 5, 7);
+    Mat22* inv_K = mat22_inv(K);
 
-  std::cout << "K :: \n";
-  mat22_print(K);
+    std::cout << "K :: \n";
+    mat22_print(K);
+    std::cout << "\n";
 
-  std::cout << "\n" << "K(-1) :: \n";
-  mat22_print(inv_K);
+    std::cout << "K(-1) :: \n";
+    mat22_print(inv_K);
+    std::cout << "\n";
 
-  // test if inverse
-  Mat22* identity = mat22_mul(K, inv_K);
-  std::cout << "Identity Matrix :: \n";
-  mat22_print(identity);
+    // test if inverse
+    Mat22* identity = mat22_mul(K, inv_K);
+    std::cout << "Identity Matrix :: \n";
+    mat22_print(identity);
+    std::cout << "\n";
+
+    free(K);
+    free(inv_K);
+    free(identity);
+  }
+
+  std::cout << "------------------------\n\n";
+
+  {
+    char* P = (char*)malloc(7);
+    memcpy(P, "meetme", 6);
+    std::cout << "Plaintext = " << P << "\n\n";
+
+    Mat22* K = mat22_set(9, 4, 5, 7);
+    std::cout << "Key = \n";
+    mat22_print(K);
+    std::cout << "\n";
+
+    char* C = hillcipher_encrypt(P, K);
+    std::cout << "Ciphertext = " << C << "\n\n";
+
+    free(P);
+    free(K);
+    free(C);
+  }
+
+  std::cout << "------------------------\n\n";
+
+  {
+    char* C = (char*)malloc(7);
+    memcpy(C, "RDJAYF", 6);
+    std::cout << "Ciphertext = " << C << "\n\n";
+
+    Mat22* K = mat22_set(9, 4, 5, 7);
+    std::cout << "Key = \n";
+    mat22_print(K);
+    std::cout << "\n";
+
+    char* P = hillcipher_decrypt(C, K);
+    std::cout << "Plaintext = " << P << "\n\n";
+  }
+
+  std::cout << "------------------------\n\n";
+
+  {
+    char* P = "howareyoutoday";
+    char* C = "ZWSENIUSPLJVEU";
+
+    Mat22* K = NULL;
+
+    Mat22* p_mat = mat22_set('h', 'o', 'w', 'a');
+    Mat22* c_mat = mat22_set('Z', 'W', 'S', 'E');
+
+    int denom = mod((p_mat->data[0][0] * p_mat->data[1][1]) - (p_mat->data[0][1] * p_mat->data[1][0]), 26);
+
+    std::cout << "If p_mat :: | h  o| then p_mat isn't invertible because the determinant of p_mat is " << denom << "\n";
+    std::cout << "            | w  a|\n";
+  }
 
   return 0;
 }
